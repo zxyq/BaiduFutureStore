64b0b811737375beb265fa496b3e7c52fbaa2d6d
zxyq <511292331@qq.com>
2016-06-27 08:43:14 +0800

    shangchuan

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(function(){
+  var mySwiper = new Swiper('.swiper-container', {
+	autoplay: 5000,
+  loop : true,
+  prevButton:'.swiper-button-prev',
+  nextButton:'.swiper-button-next',
+  pagination : '.swiper-pagination',
+})
+})
